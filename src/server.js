50da35fa6e90455dec0b06cb377d07907072ac36
unknown <akhandraj402@gmail.com>
2026-03-11 18:45:46 +0530

    insial test commits

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
-const Fastify = require('fastify');
+const Fastify = require("fastify")
 const app = Fastify()
-
-app.get("/health", async () => {
-        return {ok: true}
-})
-
 
 async function health() {
         return {ok: true}
 }
 
 app.get("/health", health)
+
+async function start() {
+        try {
+                await app.listen({port: 3000, host: "0.0.0.0"})
+                console.log("server running on port : 3000")
+        } catch (err) {
+                console.error(err)
+                process.exit(1)
+        }
+}
+start()

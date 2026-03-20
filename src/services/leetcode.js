48affd7e7f2074bdcdd69e6bc982475dfe16c718
unknown <akhandraj402@gmail.com>
2026-03-20 17:35:51 +0530

    some new unfinished work

--- src/services/leetcode.js
+++ src/services/leetcode.js
@@ -1,25 +1,67 @@
 const axios = require("axios");
 const { CONFIG } = require("../config")
-const Leetcode_APi_ENDPOINT = CONFIG['leetcode']['endpoint']
-const USERNAME = CONFIG['leetcode']['username']
+const LEETCODE_API_ENDPOINT = CONFIG.leetcode.endpoint
+const USERNAME = CONFIG.leetcode.username
 
 
 
 async function LeetcodeProfileData(username) {
-    const quarry = `
+    // takes data as dict containing {username: username,solved:{easy, medium, hard}, total: {easy, medium, hard}};
+    const query = `
     query getUserProfile($username: String!) {
+        matchedUser(username: $username){ 
+            submitStats{
+                asSubmissionNum { difficulty count }
+            }
+        }
+        allQuestionsCount {difficulty count}
+    }`;
+    const res = await axios.post(
+        LEETCODE_API_ENDPOINT,
+        { query: query, variables: { username }},
+        { headers: { "Content-Type": "application/json",} }
+    );
+
+    if (! res.ok) {
+        return null;
     }
-    `
+    if (!res.data.data.matchedUser) return null;
+    
+    const userStats = res.data.data.matchedUser.submitStats.asSubmissionNum;
+    const totalStats = res.data.data.allQuestionsCount;
+
+    const getCount = (arr, diff) => {
+        arr.find(x => x.difficulty === diff)?.count || 0;
+    }
+
+    return {
+        username: username,
+        solved: {
+            easy: getCount(userStats, "Easy"),
+            medium: getCount(userStats, "Medium"),
+            hard: getCount(userStats, "Hard"),
+        },
+        total:{
+            easy: getCount(totalStats, "Easy"),
+            medium: getCount(totalStats, "Medium"),
+            hard: getCount(totalStats, "Hard")
+        }
+    };
 }
 
-const leetcode = {
-    "easy": 300,
-    "medium": 200,
-    "hard": 200,
-    "total eassy": 1700,
-    "total medium": 1500,
-    "total hard": 700
-}
+
+
+
+
+
+
+// "leetcode": {
+//           "easy": 300,
+//           "medium": 200,
+//           "hard": 200,
+//           "totalSolved": 1400,
+//           "totalAvailable": 2300
+//         },
 
 
 // {

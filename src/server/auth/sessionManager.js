b5166c7ab4642d2f041e6d3a3f1ebd8f8c256e08
unknown <akhandraj402@gmail.com>
2026-05-14 18:43:30 +0530

    idk what to do my friend died uv he wanted to see the site and now he cant i am not sure how to feel

--- src/server/auth/sessionManager.js
+++ src/server/auth/sessionManager.js
@@ -1,4 +1,48 @@
 const crypto = require("crypto");
+const { MinQueue } = require("heapify");
+
+class sessionMap{
+    constructor({ inactiveTime, sessionEndTime, checkInterval, sessionRemoveEvent, sessionActiveCheck}){
+        this.inactiveTime = inactiveTime ?? 120000;
+        this.sessionEndTime = sessionEndTime ?? 600000;
+        this.checkInterval = checkInterval ?? 10000;
+        this.sessionRemoveEvent = sessionRemoveEvent;
+        this.sessionActiveCheck = sessionActiveCheck;
+
+        if (isNaN(sessionRemoveEvent)){
+            throw runtimeError("sessionRemoveEvent argeumnt in sessionMap is needed to start server")
+        }
+
+        if (isNaN(sessionActiveCheck)){
+            throw runtimeError("sessionRemoveEvent argeumnt in sessionMap is needed to start server")
+        }
+    }
+
+    trackSession({ sessionId }){
+        // start trakcing session
+    }
+
+    run() {
+        // start the sessionmap worker
+    }
+
+    _check_and_remove(){
+        // main egion who check if session is active or not 
+    }
+
+    // both sessionRemoveEent adn sesionActiveCheck should take sessionId as arguemnt
+    
+}
+
+
+
+
+
+
+
+
+
+
 
 
 class SessionManager {
